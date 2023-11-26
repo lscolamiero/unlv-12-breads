ebb525e50485734cdfdd138ff69149118468d28b
lscolamiero <lautaro.scolamiero@gmail.com>
2023-11-25 19:37:53 -0800

    array of objects added to bread.js

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,7 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require("../models/bread.js")
 
+//INDEX
 //http://localhost:3000/breads/
 breads.get('/', (req, res) => {
     res.send(Bread)
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+//DEPENDENCIES
 const express = require('express')
+
+// CONFIGURATION
 require('dotenv').config()
-
 const PORT = process.env.PORT
-
 const app = express()
 
+//ROUTES
 app.get('/', (req, res) => {
     res.send("Welcome to an Awesome App about Breads!")
 });
@@ -12,6 +14,7 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+//LISTEN
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
 })
