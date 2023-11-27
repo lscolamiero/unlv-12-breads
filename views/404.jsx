8462ce22fe12ba998a2b2db348061d31c8357081
lscolamiero <lautaro.scolamiero@gmail.com>
2023-11-27 14:33:59 -0800

    create and new routes complete, fixed error with 404 view

--- views/404.jsx
+++ views/404.jsx
@@ -1,10 +1,11 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function errorNotfound (title) {
+function errorNotfound () {
         return (
-            <Default title={title}>
+            <Default>
                 <h2>404 Error Snap!</h2>
+                <p>This means the bread you are looking for is not available.Sorry for the inconvenience.</p>
                 <img src="https://images.unsplash.com/photo-1584824486509-112e4181ff6b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="404 error" />
                 <li><a href="/breads">Go home</a></li>
             </Default>
