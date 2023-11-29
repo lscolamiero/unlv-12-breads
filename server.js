6735aa2619ddfbc2b38f38854ba4ba46eaab5f3b
lscolamiero <lautaro.scolamiero@gmail.com>
2023-11-28 19:32:51 -0800

    delete route complete

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 //DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
-
 const PORT = process.env.PORT
-
 const app = express()
 
 // MIDDLEWARE
@@ -14,7 +13,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-
+app.use(methodOverride('_method'))
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -24,7 +23,7 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
-// 404 Page
+//404 Page
 app.get('*', (req, res) => {
     res.render('/views/404')
   })  
