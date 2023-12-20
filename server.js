9535adb80ebff6161dc1fc8c3fccbe2ca467db78
lscolamiero <lautaro.scolamiero@gmail.com>
2023-12-19 21:03:01 -0800

    exported the app express for render deployment

--- server.js
+++ server.js
@@ -40,3 +40,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
 })
+
+module.exports = app;
